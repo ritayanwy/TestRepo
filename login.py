5fdc073d9f0e479ffde932a6ca900966cbcab2c6
anurag <anurag.shukla@webyog.com>
2014-10-14 17:31:30 +0530

    added login

---
+++ login.py
@@ -0,0 +1,34 @@
+import json
+from testcases import TEST_ENV
+
+
+def handler(response):
+    print response.text
+    if json.loads(response.text)['error_code'] != 0:
+        return False
+    TEST_ENV['global_headers']['X-CMSESSION'] = json.loads(response.text)['data']['access_tokens']['CMSESSION']
+    TEST_ENV['global_headers']['X-CMSID'] = json.loads(response.text)['data']['access_tokens']['CMSID']
+    return True
+
+
+TEST = {
+        'name': 'Login API',
+        'request': [
+        {
+                'method': 'POST',
+                'url': '/k/v5/user/session_create',
+                'timeout': 60,
+                'data': {'email': 'XX@CC.com',
+                         'password': 'QQQQQQ'}
+        }
+        ],
+        'response': [
+        {
+                'status_code': 200,
+                'hooks': handler
+        }
+        ]
+}
+
+
+
